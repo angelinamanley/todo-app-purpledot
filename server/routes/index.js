52e793f0931f75fbd09f53c227d5085ccda3c4fa
Angelina Shin <angelinashin@angelinas-mbp.home>
2021-10-13 16:32:34 +0100

    add routes excl patch tasks

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,4 +11,36 @@ router.get("/tasks", (req, res) => {
   });
 });
 
+router.get("/tasks/:id", (req, res) => {
+  const id = req.params.id;
+  const query = "SELECT * FROM Task where task_id = ?";
+  connection.query(query, id, function (err, result) {
+    if (err) {
+      res.status(400).send("Unable to retrieve task.");
+    }
+    res.send(result);
+  });
+});
+
+router.post("/tasks", (req, res) => {
+  const query = "INSERT INTO Task SET ?";
+  connection.query(query, req.body, function (err, result) {
+    if (err) {
+      return res.status(400).send("Unable to create new task!");
+    }
+    res.send(result);
+  });
+});
+
+router.delete("/tasks/:id", (req, res) => {
+  const id = req.params.id;
+  const query = "DELETE FROM Task where task_id = ?";
+  connection.query(query, [id], (err, results) => {
+    if (err) {
+      return res.status(500).send();
+    }
+    res.send({ success: true });
+  });
+});
+
 module.exports = router;
