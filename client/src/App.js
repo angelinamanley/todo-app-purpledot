7d39ec767903357f62a31ad77feb804ca64facb4
Angelina Shin <angelinashin@angelinas-mbp.home>
2021-10-13 16:41:10 +0100

    set up react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "semantic-ui-react";
+
+const tasks = [{ task: "do this", completed: 0, task_id: 1 }];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
