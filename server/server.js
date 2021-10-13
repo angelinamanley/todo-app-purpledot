0a7c21ec21d577f1ed1738a9d7b699b2567af10b
Angelina Shin <angelinashin@angelinas-mbp.home>
2021-10-13 17:10:01 +0100

    set up cors for express, create fetches in client

--- server/server.js
+++ server/server.js
@@ -3,9 +3,14 @@ require("./db");
 
 const app = express();
 const router = require("./routes");
+const cors = require('cors')
 
 const port = process.env.port || 4000;
-
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, 
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
